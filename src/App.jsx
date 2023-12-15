44bb3566c2c92301fdbba647145759a2097f8caf
ankitapriya108 <139537048+ankitapriya108@users.noreply.github.com>
2023-12-15 10:37:31 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='navbar'>
+        <h2>LOGO</h2>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+        <button><a href="">Login</a></button>
+      </div>
+    </>
+  )
+}
+
+export default App
